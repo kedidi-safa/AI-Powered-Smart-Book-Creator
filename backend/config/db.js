d7632b7b502e314a2478265d6f12f02021a7620f
kedidi-safa <safakedidi3@gmail.com>
2026-04-12 18:37:15 +0200

    deploy to ec2

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 
+const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
+const connectionString = `mongodb+srv://test:${password}@smartbookcreator.7zcnjf7.mongodb.net/?appName=smartBookCreator`;
+
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {});
-        console.log("MongoDB connected");
-    } catch (err) {
-        console.error("Error connecting to MongoDB", err);
-        process.exit(1)
-    }
+  try {
+    await mongoose.connect(connectionString, {});
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("Error connecting to MongoDB", err);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
